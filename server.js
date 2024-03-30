7b2e7e5db9517e66402ef086a159e0218644eff2
HaopengSun <sunhaopeng2016@gmail.com>
2024-03-29 23:24:26 -0400

    webpack integration

--- server.js
+++ server.js
@@ -15,3 +15,19 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+// In development environment
+if (process.env.NODE_ENV === 'development') {
+    const webpack = require('webpack');
+    const webpackDevMiddleware = require('webpack-dev-middleware');
+    const webpackHotMiddleware = require('webpack-hot-middleware');
+    const config = require('./webpack.config.js');
+
+    const compiler = webpack(config);
+
+    app.use(webpackDevMiddleware(compiler, {
+        publicPath: config.output.publicPath
+    }));
+
+    app.use(webpackHotMiddleware(compiler));
+}
